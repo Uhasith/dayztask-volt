9ae74dec6747075b2de0733e1692befac632a233
Hasith Udayanga <uhasith5@gmail.com>
2024-11-16 22:38:03 +0530

    auth config change

--- config/devdojo/auth/settings.php
+++ config/devdojo/auth/settings.php
@@ -9,7 +9,7 @@ return [
     'registration_include_name_field' => true,
     'registration_include_password_confirmation_field' => true,
     'registration_require_email_verification' => true,
-    'enable_branding' => true,
+    'enable_branding' => false,
     'dev_mode' => false,
     'enable_2fa' => false, // Enable or disable 2FA functionality globally
     'login_show_social_providers' => true,
