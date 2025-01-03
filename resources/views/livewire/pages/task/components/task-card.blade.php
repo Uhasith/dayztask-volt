ebfc3fec083a6247fcbc8a2a96b13dacfebcc5e2
Hasith Udayanga <uhasith5@gmail.com>
2025-01-03 12:19:52 +0530

    UI fixes

--- resources/views/livewire/pages/task/components/task-card.blade.php
+++ resources/views/livewire/pages/task/components/task-card.blade.php
@@ -1,8 +1,8 @@
 <div x-init="initFlowbite();">
     <x-wui-card rounded="3xl" class="cursor-pointer" wire:poll.10s="fetchData">
         <div class="flex items-center justify-between">
-            <div class="max-w-[80%]">
-                <a href="{{ route('projects.tasks.update', $task['uuid']) }}" class="text-lg font-semibold truncate" wire:navigate>{{ $task['name'] }}</a>
+            <div class="max-w-md truncate">
+                <a href="{{ route('projects.tasks.update', $task['uuid']) }}" class="text-lg font-semibold" wire:navigate>{{ $task['name'] }}</a>
             </div>
 
             <div class="flex items-center gap-4">
