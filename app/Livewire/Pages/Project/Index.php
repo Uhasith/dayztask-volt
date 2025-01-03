2b5c82989d936c7d54aef42c524e5b28381fd711
Hasith Udayanga <uhasith5@gmail.com>
2025-01-03 10:36:26 +0530

    Fix project icon loading and bg color

--- app/Livewire/Pages/Project/Index.php
+++ app/Livewire/Pages/Project/Index.php
@@ -3,9 +3,10 @@
 namespace App\Livewire\Pages\Project;
 
 use App\Models\Project;
-use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
 use Livewire\WithPagination;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 
 class Index extends Component
 {
@@ -16,6 +17,13 @@ class Index extends Component
     public function render()
     {
         $projects = Project::where('workspace_id', Auth::user()->current_workspace_id)->orderBy('created_at', 'asc')->paginate(12);
+
+        // Attach the first logo URL to each project
+        foreach ($projects as $project) {
+            $firstMediaUrl = $project->getFirstMediaUrl('company_logo');
+            Log::info($firstMediaUrl ?: 'No logo found'); // Log the result
+            $project->company_logo = $firstMediaUrl ?: null; // Assign null if no logo is found
+        }
 
         return view('livewire.pages.project.index', [
             'projects' => $projects,
