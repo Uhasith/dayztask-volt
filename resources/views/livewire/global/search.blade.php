bbbd9018783262c9b7a8a2970ade704bab839a3c
Hasith Udayanga <uhasith5@gmail.com>
2025-01-06 12:28:27 +0530

    Fix tasks global search

--- resources/views/livewire/global/search.blade.php
+++ resources/views/livewire/global/search.blade.php
@@ -19,12 +19,12 @@ new class extends Component {
                 ->toArray(),
         )
             ->where('name', 'like', '%' . $this->search . '%')
-            ->limit(5)
+            ->limit(15)
             ->get();
         $projects = Project::where('workspace_id', Auth::user()->current_workspace_id)
             ->orderBy('created_at', 'asc')
             ->where('title', 'like', '%' . $this->search . '%')
-            ->limit(5)
+            ->limit(15)
             ->get();
 
         // Format tasks and projects with icons
